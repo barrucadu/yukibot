969592402d62aab724f8ea98b19652d17b0ffe6d
Michael Walker <mike@barrucadu.co.uk>
2014-08-16 20:39:55 +0100

    Decompose the state into mutable and immutable bits, with a message log, and put into RWST

--- irclib/Network/IRC/IDTE.hs
+++ irclib/Network/IRC/IDTE.hs
@@ -10,19 +10,23 @@ module Network.IRC.IDTE
     ) where
 
 import Control.Applicative    ((<$>))
-import Control.Monad          (forever)
+import Control.Monad          (forever, void)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.RWS (ask, get, runRWST, tell)
 import Crypto.Random.AESCtr   (makeSystem)
 import Data.ByteString        (ByteString)
-import Data.ByteString.Char8  (pack)
+import Data.ByteString.Char8  (pack, unpack)
 import Data.ByteString.Lazy   (fromChunks)
 import Data.Default           (def)
 import Data.Text.Encoding     (encodeUtf8)
+import Data.Time.Clock        (getCurrentTime)
+import Data.Time.Format       (formatTime)
 import Network                (HostName, PortID, connectTo)
 import Network.IRC            (Message, encode, decode)
-import Network.IRC.IDTE.Types (IRC(..), IRCConf(..))
+import Network.IRC.IDTE.Types
 import Network.TLS
 import Network.TLS.Extra      (ciphersuite_all)
+import System.Locale          (defaultTimeLocale)
 import System.IO
 
 import qualified Network.IRC as I
@@ -30,8 +34,8 @@ import qualified Network.IRC as I
 -- *Connecting to an IRC network
 
 -- |Connect to a server without TLS.
-connect :: MonadIO m => IRCConf -> HostName -> PortID -> m IRC
-connect conf host port = do
+connect :: MonadIO m => HostName -> PortID -> m ConnectionConfig
+connect host port = do
   h <- liftIO $ do
     h' <- connectTo host port
     hSetEncoding    h' utf8
@@ -39,27 +43,23 @@ connect conf host port = do
     hSetNewlineMode h' nativeNewlineMode
     return h'
 
-  return IRC { _handle   = h
+  return ConnectionConfig
+             { _handle   = h
              , _tls      = Nothing
              , _server   = host
              , _port     = port
-             , _nick     = _cnick conf
-             , _username = _cusername conf
-             , _realname = _crealname conf
-             , _channels = []
-             , _ctcpVer  = _cctcpver conf
              }
 
 -- |Connect to a server with TLS.
-connectWithTLS :: MonadIO m => IRCConf -> HostName -> PortID -> m IRC
-connectWithTLS conf host port = connectWithTLS' conf host port defaultCiphers
+connectWithTLS :: MonadIO m => HostName -> PortID -> m ConnectionConfig
+connectWithTLS host port = connectWithTLS' host port defaultCiphers
 
 -- |Connect to a server without TLS, supplying your own list of
 -- ciphers, ordered by preference.
-connectWithTLS' :: MonadIO m => IRCConf -> HostName -> PortID -> [Cipher] -> m IRC
-connectWithTLS' conf host port ciphers = do
+connectWithTLS' :: MonadIO m => HostName -> PortID -> [Cipher] -> m ConnectionConfig
+connectWithTLS' host port ciphers = do
   -- Get an unencrypted connection
-  irc <- connect conf host port
+  irc <- connect host port
 
   -- And add a TLS context to it
   -- TODO: Do we want a better way to choose the bytes?
@@ -71,29 +71,49 @@ connectWithTLS' conf host port ciphers = do
 -- them off to handlers as appropriate.
 -- 
 -- TODO: Any of that.
-run :: (Functor m, MonadIO m) => IRC -> m ()
-run irc = do
+run :: MonadIO m => ConnectionConfig -> InstanceConfig -> m ()
+run cconf iconf = liftIO . void $ runRWST runner cconf iconf
+
+-- |The event loop.
+runner :: IRC ()
+runner = do
   -- Set the nick
   -- TODO: Mangle the nick until we get a unique one
-  send irc . I.nick . encodeUtf8 . _nick $ irc
+  send . I.nick . encodeUtf8 . _nick <$> get
 
   -- Event loop
-  liftIO . forever $ do
-    msg <- recv irc
-    -- TODO: Give to event handler (todo: event handlers)
-    liftIO $ print msg
+  forever $ do
+    msg <- recv
+    case msg of
+      -- TODO: Give to event handler (todo: event handlers)
+      Just msg' -> logmsg msg'
+
+      -- Ignore malformed messages
+      Nothing   -> return ()
 
 -- *Disconnecting
 
 -- |Disconnect from a server, properly tearing down the TLS session
 -- (if there is one).
-disconnect :: MonadIO m => IRC -> m ()
-disconnect irc = do
-  case _tls irc of
+disconnect :: IRC ()
+disconnect = do
+  tls <- _tls    <$> ask
+  h   <- _handle <$> ask
+
+  case tls of
     Just ctx -> bye ctx
     Nothing  -> return ()
 
-  liftIO . hClose . _handle $ irc
+  liftIO $ hClose h
+
+-- |Log a message to stdout and the internal log
+logmsg :: Message -> IRC ()
+logmsg msg = do
+  now <- liftIO $ getCurrentTime
+
+  tell [(now, encode msg)]
+
+  liftIO . putStrLn $ formatTime defaultTimeLocale "%c" now ++ unpack (encode msg)
 
 -- *TLS
 
@@ -116,16 +136,24 @@ addTLS host bytes h ciphers = do
 
 -- |Send a plain message
 -- TODO: Flood control
-send :: MonadIO m => IRC -> Message -> m ()
-send irc msg = let h    = _handle irc
-                   msg' = encode msg
-               in case _tls irc of
-                    Just ctx -> sendData ctx $ fromChunks [msg']
-                    Nothing  -> liftIO $ hPrint h msg' >> hPrint h "\r\n"
+send :: Message -> IRC ()
+send msg = do
+  tls <- _tls    <$> ask
+  h   <- _handle <$> ask
+
+  let msg' = encode msg
+
+  case tls of
+    Just ctx -> sendData ctx $ fromChunks [msg']
+    Nothing  -> liftIO $ hPrint h msg' >> hPrint h "\r\n"
 
 -- |Receive a plain message. This blocks.
-recv :: (Functor m, MonadIO m) => IRC -> m (Maybe Message)
-recv irc = decode <$>
-             case _tls irc of
-               Just ctx -> recvData ctx
-               Nothing  -> pack <$> (liftIO . hGetLine . _handle $ irc)
+recv :: IRC (Maybe Message)
+recv = do
+  tls <- _tls    <$> ask
+  h   <- _handle <$> ask
+
+  decode <$>
+    case tls of
+      Just ctx -> recvData ctx
+      Nothing  -> pack <$> liftIO (hGetLine h)
