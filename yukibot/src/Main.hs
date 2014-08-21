99348792d2ae358febce94779c1e5298c0330e22
Michael Walker <mike@barrucadu.co.uk>
2014-08-21 20:12:08 +0100

    Add a demonstration of network joining to the Yukibot demo

--- yukibot/src/Main.hs
+++ yukibot/src/Main.hs
@@ -6,26 +6,43 @@ import Control.Applicative ((<$>))
 import Control.Monad       (void)
 import Data.Monoid         ((<>))
 import Data.Text           (pack)
+import Network             (HostName)
 import Network.IRC.Asakura
+import Network.IRC.Asakura.Types
 import Network.IRC.IDTE
 
 main :: IO ()
 main = do
-  let iconf  = defaultIRCConf "yukibot"
-  let iconf' = iconf { _eventHandlers = EventHandler "Reply to everything" EEverything bounceBack : _eventHandlers iconf }
-  void $ createAndRun "irc.freenode.net" 6667 (Right iconf')
+  cconf <- connect "irc.freenode.net" 6667
+  state <- newBotState
 
-bounceBack :: Event -> IRC ()
-bounceBack ev = do
-  nck <- _nick <$> instanceConfig
+  addGlobalEventHandler' state $ AsakuraEventHandler "Respond to everything" EEverything bounceBack runEverywhere runAlways
+
+  case cconf of
+    Right cconf' -> void $ run cconf' (defaultIRCConf "yukibot") state
+    Left err     -> putStrLn err
+
+bounceBack :: IRCState -> Event -> Bot (IRC ())
+bounceBack ircstate ev = do
+  nck <- _nick <$> getInstanceConfig' ircstate
+  let net = _server $ getConnectionConfig ircstate
+
   case _eventType ev of
-    ENotice -> return () -- Notices don't produce automatic responses.
+    ENotice -> return $ return () -- Notices don't produce automatic responses.
     _       -> case _source ev of
-                Channel n c | n /= nck   -> send $ privmsg c $ n <> ": " <> msg
-                            | otherwise -> return () -- Don't reply to messages from self.
-                User    n   | n /= nck   -> send $ query n msg
-                            | otherwise -> return ()
-                _                       -> return ()
+                Channel n c | n /= nck   -> return . send $ privmsg c $ n <> ": " <> msg
+                            | otherwise -> return $ return () -- Don't reply to messages from self.
+                User    n   | n == "barrucadu" && net /= "irc.rizon.net" -> joinRizon
+                            | n /= nck   -> return . send $ query n msg
+                            | otherwise -> return $ return ()
+                _                       -> return $ return ()
 
     where msg = pack . show . _message $ ev
 
+joinRizon :: Bot (IRC ())
+joinRizon = do
+  cconf <- connect "irc.rizon.net" 6667
+  case cconf of
+    Right cconf' -> addNetwork cconf' $ defaultIRCConf "yukibot"
+    Left err -> error err
+  return $ return ()
