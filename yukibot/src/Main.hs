0716acbe99f1ac9774506a70d5f15bd317c7d04b
Michael Walker <mike@barrucadu.co.uk>
2014-08-23 23:19:27 +0100

    Split out the Yukibot state into a separate module

--- yukibot/src/Main.hs
+++ yukibot/src/Main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- TODO: Catch SIGTERM and disconnect from all networks.
 module Main where
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>))
 import Control.Monad       (void)
-import Data.Aeson          (FromJSON(..), ToJSON(..), Value(..), (.=), (.:), object, eitherDecode)
-import Data.Aeson.Encode.Pretty (Config(..), encodePretty')
 import Data.Monoid         ((<>))
 import Data.Text           (Text, pack)
 import Network             (HostName)
@@ -14,85 +13,48 @@ import Network.IRC.Asakura.State (snapshot, rollback)
 import Network.IRC.Asakura.Types
 import Network.IRC.IDTE
 import System.Directory    (doesFileExist)
+import System.Exit         (exitFailure)
+import Yukibot.State
 
-import qualified Data.ByteString.Lazy            as B
 import qualified Network.IRC.Asakura.Commands    as C
 import qualified Network.IRC.Asakura.Permissions as P
 
-data YukiState = YukiState
-    { _commandState    :: C.CommandStateSnapshot
-    , _permissionState :: P.PermissionStateSnapshot
-    }
-
-instance ToJSON YukiState where
-    toJSON ys = object [ "prefixes"    .= toJSON (_commandState    ys)
-                       , "permissions" .= toJSON (_permissionState ys)
-                       ]
-
-instance FromJSON YukiState where
-    parseJSON (Object v) = YukiState
-                             <$> v .: "prefixes"
-                             <*> v .: "permissions"
-    parseJSON _ = fail "Expected object"
-
+-- |Load the configuration file, if it exists, otherwise initialise a
+-- new state. Upon successfully constructing a state, run the bot.
 main :: IO ()
 main = do
   confExists <- doesFileExist "yukibot.json"
-  if confExists
-  then runWithStateFrom "yukibot.json"
-  else runWithFreshState
+  ys <- if confExists
+       then stateFromFile "yukibot.json"
+       else Just <$> initialise
 
-runWithStateFrom :: String -> IO ()
-runWithStateFrom fn = do
-  json <- B.readFile fn
-  case eitherDecode json of
-    Right ys -> runWithState $ Just ys
-    Left _   -> putStrLn "Failed to parse configuration file"
+  case ys of
+    Just ys' -> runWithState ys'
+    Nothing  -> putStrLn "Failed to parse configuration file." >> exitFailure
 
-runWithFreshState :: IO ()
-runWithFreshState = runWithState Nothing
-
-runWithState :: Maybe YukiState -> IO ()
+-- |Run the bot with a given state.
+runWithState :: YukibotState -> IO ()
 runWithState ys = do
   cconf <- connect "irc.freenode.net" 6667
   state <- newBotState
-  (permissionState, commandState) <- initState
+
+  let commandState    = _commandState ys
+  let permissionState = _permissionState ys
 
   C.registerCommand commandState "echo"  Nothing            bounceBack
   C.registerCommand commandState "rizon" (Just P.God)       joinRizon
   C.registerCommand commandState "join"  (Just $ P.Admin 0) joinChannel
   C.registerCommand commandState "part"  (Just $ P.Admin 0) partChannel
-  C.registerCommand commandState "quit"  (Just P.God)     quitNetwork
+  C.registerCommand commandState "quit"  (Just P.God)       quitNetwork
 
   addGlobalEventHandler' state $ C.eventRunner commandState
 
   case cconf of
     Right cconf' -> do
       void $ run cconf' (defaultIRCConf "yukibot") state
-      css <- snapshot commandState
-      pss <- snapshot permissionState
-      let ys = YukiState { _commandState    = css
-                         , _permissionState = pss
-                         }
+      save "yukibot.json" ys
 
-      let bs = encodePretty' (Config 4 compare) ys
-      B.writeFile "yukibot.json" bs
-    Left err     -> putStrLn err
-
-  where initState = case ys of
-                      Just ys' -> do
-                        permissionState <- rollback $ _permissionState ys'
-                        commandState    <- rollback $ _commandState ys'
-                        return (permissionState, commandState permissionState)
-
-                      Nothing -> do
-                        permissionState <- P.initialise
-                        commandState    <- C.initialise "#" permissionState
-
-                        P.setNetworkPermission permissionState "barrucadu" "irc.freenode.net" P.God
-
-                        return (permissionState, commandState)
-
+    Left err -> putStrLn err >> exitFailure
 
 bounceBack :: [Text] -> IRCState -> Event -> Bot (IRC ())
 bounceBack _ _ ev = case _source ev of
---
+++ yukibot/src/Yukibot/State.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+
+module Yukibot.State where
+
+import Control.Applicative      ((<$>), (<*>))
+import Control.Concurrent.STM   (atomically)
+import Control.Monad            (join)
+import Control.Monad.IO.Class   (MonadIO, liftIO)
+import Data.Aeson               (FromJSON(..), ToJSON(..), Value(..), (.=), (.:), object, decode')
+import Data.Aeson.Encode.Pretty (Config(..), encodePretty')
+import Data.ByteString.Lazy     (ByteString)
+import Data.Text                ()
+import Network.IRC.Asakura.State
+import System.Directory         (doesFileExist)
+
+import qualified Data.ByteString.Lazy            as B
+import qualified Network.IRC.Asakura.Commands    as C
+import qualified Network.IRC.Asakura.Permissions as P
+
+-- *Live State
+
+-- |The live state of the bot.
+data YukibotState = YS
+    { _commandState    :: C.CommandState
+    , _permissionState :: P.PermissionState
+    }
+
+-- *Snapshotting
+
+-- |A snapshot of the bot.
+data YukibotStateSnapshot = YSS
+    { _commandSnapshot    :: C.CommandStateSnapshot
+    , _permissionSnapshot :: P.PermissionStateSnapshot
+    }
+
+instance Snapshot YukibotState YukibotStateSnapshot where
+    snapshotSTM ys = do
+      css <- snapshotSTM . _commandState    $ ys
+      pss <- snapshotSTM . _permissionState $ ys
+
+      return YSS { _commandSnapshot    = css
+                 , _permissionSnapshot = pss
+                 }
+
+instance Rollback YukibotStateSnapshot YukibotState where
+    rollbackSTM yss = do
+      cs <- rollbackSTM . _commandSnapshot    $ yss
+      ps <- rollbackSTM . _permissionSnapshot $ yss
+
+      return YS { _commandState    = cs ps
+                , _permissionState = ps
+                }
+
+instance ToJSON YukibotStateSnapshot where
+    toJSON yss = object [ "prefixes"    .= toJSON (_commandSnapshot yss)
+                        , "permissions" .= toJSON (_permissionSnapshot yss)
+                        ]
+
+instance FromJSON YukibotStateSnapshot where
+    parseJSON (Object v) = YSS <$> v .: "prefixes"
+                               <*> v .: "permissions"
+    parseJSON _ = fail "Expected object"
+
+-- *Initialisation
+
+-- |Construct a new state, with a default command prefix of "!".
+initialise :: MonadIO m => m YukibotState
+initialise = do
+  ps <- P.initialise
+  cs <- C.initialise "!" ps
+
+  return YS { _commandState    = cs
+            , _permissionState = ps
+            }
+
+-- |Attempt to load a state from a lazy ByteString.
+stateFromByteString :: (Functor m, MonadIO m) => ByteString -> m (Maybe YukibotState)
+stateFromByteString bs = case (decode' bs :: Maybe YukibotStateSnapshot) of
+                           Just yss -> Just <$> rollback yss
+                           Nothing  -> return Nothing
+
+-- |Attempt to load a state from a file.
+stateFromFile :: MonadIO m => FilePath -> m (Maybe YukibotState)
+stateFromFile fp = liftIO $ do
+  exists <- doesFileExist fp
+  if exists
+  then join $ stateFromByteString <$> B.readFile fp
+  else return Nothing
+
+-- *Saving
+
+-- |Snapshot the state and save it to disk.
+save :: (Functor m, MonadIO m) => FilePath -> YukibotState -> m ()
+save fp ys = join $ saveSnapshot fp <$> snapshot ys
+
+-- |Save a snapshot of the state to disk.
+saveSnapshot :: MonadIO m => FilePath -> YukibotStateSnapshot -> m ()
+saveSnapshot fp yss = liftIO $ B.writeFile fp bs
+    where bs = encodePretty' (Config 4 compare) yss
