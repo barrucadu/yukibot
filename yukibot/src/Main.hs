bc701dd5db2a02be7be657e61770b4d1b81f3bab
Michael Walker <mike@barrucadu.co.uk>
2014-08-24 18:03:26 +0100

    Allow the configuration file to be specified on the command line

--- yukibot/src/Main.hs
+++ yukibot/src/Main.hs
@@ -9,6 +9,7 @@ import Network.IRC.Asakura
 import Network.IRC.Asakura.Types
 import Network.IRC.IDTE
 import System.Directory    (doesFileExist)
+import System.Environment  (getArgs)
 import System.Exit         (exitFailure)
 import Yukibot.State
 
@@ -17,22 +18,32 @@ import qualified Network.IRC.Asakura.Permissions as P
 import qualified Yukibot.Plugins.Channels        as CH
 import qualified Yukibot.Plugins.LinkInfo        as L
 
+-- |Default configuration file name
+defaultConfigFile :: FilePath
+defaultConfigFile = "yukibot.json"
+
 -- |Load the configuration file, if it exists, otherwise initialise a
 -- new state. Upon successfully constructing a state, run the bot.
 main :: IO ()
 main = do
-  confExists <- doesFileExist "yukibot.json"
+  configFile <- do
+    args <- getArgs
+    return $ case args of
+      (cfg:_) -> cfg
+      _       -> defaultConfigFile
+
+  confExists <- doesFileExist configFile
   ys <- if confExists
-       then stateFromFile "yukibot.json"
+       then stateFromFile configFile
        else Just <$> initialise
 
   case ys of
-    Just ys' -> runWithState ys'
+    Just ys' -> runWithState configFile ys'
     Nothing  -> putStrLn "Failed to parse configuration file." >> exitFailure
 
 -- |Run the bot with a given state.
-runWithState :: YukibotState -> IO ()
-runWithState ys = do
+runWithState :: FilePath -> YukibotState -> IO ()
+runWithState fp ys = do
   cconf <- connect "irc.freenode.net" 6667
   state <- newBotState
 
@@ -49,6 +60,6 @@ runWithState ys = do
   case cconf of
     Right cconf' -> do
       void $ run cconf' (defaultIRCConf "yukibot") state
-      save "yukibot.json" ys
+      save fp ys
 
     Left err -> putStrLn err >> exitFailure
