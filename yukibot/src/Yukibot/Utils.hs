3719f42fa5883fdedc9a9b2a8ec4e9cfa0a32e52
Michael Walker <mike@barrucadu.co.uk>
2014-08-27 19:37:52 +0100

    Use Wreq instead of Curl for http stuff

--- yukibot/src/Yukibot/Utils.hs
+++ yukibot/src/Yukibot/Utils.hs
@@ -1,54 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |Common utility functions for plugins.
 module Yukibot.Utils where
 
-import Control.Arrow          ((***))
+import Control.Applicative    ((<$>))
+import Control.Exception      (catch)
+import Control.Lens           ((&), (.~), (^.))
 import Control.Monad          (guard)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.Char              (toLower)
-import Data.List              (isInfixOf)
+import Data.ByteString        (ByteString, isInfixOf)
+import Data.ByteString.Lazy   (toStrict)
 import Data.Maybe             (fromMaybe)
 import Data.String            (IsString(..))
-import Network.Curl           (CurlBuffer, CurlOption(..), CurlResponse_(..), withCurlDo, curlGetResponse_)
+import Data.Text              (unpack)
+import Data.Text.Encoding     (decodeUtf8)
+import Network.HTTP.Client    (HttpException)
+import Network.Wreq
 import Network.URI            (URI(..), URIAuth(..), uriToString)
 
 -- |Download an HTML document. Return (Just html) if we get a 200
 -- response code, and a html-y content-type. This follows redirects.
+--
+-- This decodes the result as UTF-8. If another encoding is desired,
+-- use 'fetchHttp' directly.
 fetchHtml :: MonadIO m => URI -> m (Maybe String)
-fetchHtml = flip fetchHtml' []
+fetchHtml = flip fetchHtml' defaults
 
 -- |Like 'fetchHtml', but accepts a username and password.
+--
+-- This decodes the result as UTF-8. If another encoding is desired,
+-- use 'fetchHttp' directly.
 fetchHtmlWithCreds :: MonadIO m => URI -> String -> String -> m (Maybe String)
-fetchHtmlWithCreds url user pass = fetchHtml' url [ CurlUserPwd $ user ++ ":" ++ pass ]
+fetchHtmlWithCreds url user pass = fetchHtml' url opts
+    where opts = defaults & auth .~ basicAuth (fromString user) (fromString pass)
 
 -- |Like 'fetchHtml', but takes options (in addition to following
 -- redirects).
-fetchHtml' :: MonadIO m => URI -> [CurlOption] -> m (Maybe String)
+--
+-- This decodes the result as UTF-8. If another encoding is desired,
+-- use 'fetchHttp' directly.
+fetchHtml' :: MonadIO m => URI -> Options -> m (Maybe String)
 fetchHtml' url opts = do
   res <- fetchHttp' url opts
 
   return $ do
     response <- res
 
-    guard $ case lookup "content-type" $ map (map toLower *** map toLower) $ respHeaders response of
-              Just ctype -> "html" `isInfixOf` ctype
-              Nothing    -> False
+    guard $ "html" `isInfixOf` (response ^. responseHeader "Content-Type")
 
-    Just $ respBody response
+    Just . unpack . decodeUtf8 $ response ^. responseBody
 
 -- |Download something over HTTP, returning (Just response) on a 200
 -- response code. This follows redirects.
-fetchHttp :: (CurlBuffer c, MonadIO m) => URI -> m (Maybe (CurlResponse_ [(String, String)] c))
-fetchHttp = flip fetchHttp' []
+fetchHttp :: MonadIO m => URI -> m (Maybe (Response ByteString))
+fetchHttp = flip fetchHttp' defaults
 
 -- |Like 'fetchHttp', but also takes options (in addition to following
 -- redirects).
-fetchHttp' :: (CurlBuffer c, MonadIO m) => URI -> [CurlOption] -> m (Maybe (CurlResponse_ [(String, String)] c))
-fetchHttp' url opts = liftIO . withCurlDo $ do
-  res <- curlGetResponse_ (showUri url) $ CurlFollowLocation True : opts
+fetchHttp' :: MonadIO m => URI -> Options -> m (Maybe (Response ByteString))
+fetchHttp' url opts = liftIO $ fetch `catch` handler
+    where fetch = do
+            res <- getWith (opts & redirects .~ 10) $ showUri url
 
-  return $ if respStatus res == 200
-           then Just res
-           else Nothing
+            return $ if res ^. responseStatus . statusCode == 200
+                     then Just $ toStrict <$> res
+                     else Nothing
+
+          handler = const $ return Nothing :: HttpException -> IO (Maybe (Response ByteString))
 
 -- |Convert a URI into a string-like thing.
 showUri :: IsString s => URI -> s
