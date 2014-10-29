cc22251eeb399dd149787e4eb58e17b5ff955bde
Nathan van Doorn <nvd1234@gmail.com>
2014-10-29 00:38:53 +0000

    Added brainfuck plugin. Untested.

---
+++ yukibot/src/Yukibot/Plugins/Brainfuck.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Yukibot.Plugins.Brainfuck (command) where
+
+import Control.Applicative ((<$))
+import Control.Lens
+import Control.Monad.Trans.State    (State, modify)
+import qualified Data.DList as D
+import Data.Text                    (Text)
+import qualified Data.Text as T
+import Network.IRC.Asakura.Commands (CommandDef(..))
+import Network.IRC.Client           (reply)
+import Text.Parsec
+import Text.Parsec.Text
+import Yukibot.Utils.Types          (Zipper(..), left, right, focus)
+
+-- |Data type representing a single brainfuck element, that is, a single command
+-- or a loop.
+--
+-- Names are abbreviated for brevity and to avoid clashing with Prelude. (I'm
+-- looking at you, Either....)
+--
+-- Multiple consecutive +s and -s are combined.
+data BFElement = L | R | P Int | I | O | Loop [BFElement]
+
+-- |A brainfuck program is just a list of brainfuck elements.
+type BFProgram = [BFElement]
+
+-- |The state of a brainfuck program is its tape, its input, and its output.
+data BFState = BFState { _tape   :: Zipper Int
+                       , _input  :: [Char]
+                       , _output :: D.DList Char
+                       }
+makeLenses ''BFState
+
+defaultBFState = BFState { _tape = Z (repeat 0) 0 (repeat 0)
+                         , _input = ""
+                         , _output = D.empty
+                         }
+
+consumeElement :: BFElement -> State BFState ()
+consumeElement (P z) = tape.focus += z
+consumeElement L = tape %= left
+consumeElement R = tape %= right
+consumeElement I = tape.focus.enum <~ input %%= fromMaybe ('\0', "") . uncons
+consumeElement O = do
+  c <- use (tape.focus)
+  output <>= D.singleton c
+consumeElement (Loop l) = go
+  where
+    go = do
+      c <- use (tape.focus)
+      when (c /= 0) $ do
+        mapM_ consumeElement l
+        go
+
+consumeProgram :: BFProgram -> State BFState ()
+consumeProgram = mapM_ consumeElement
+
+runProgram :: BFProgram -> [Char] -> DList Char
+runProgram program i = _output $ execState (consumeProgram program)
+                       defaultBFState {_input = i}
+
+parseProgram :: Monad m => Parser m BFProgram
+parseProgram = comment >> many $ choice [
+  parsePM,
+  parseLoop,
+  L <$ char '<',
+  R <$ char '>',
+  I <$ char ',',
+  O <$ char '.'] >> comment
+  where
+    parsePM = fmap (P . sum) $ many1 $ choice [
+      1    <$ char '+',
+      (-1) <$ char '-']
+    parseLoop = fmap Loop $ between (char '[') (char ']') parseProgram
+    comment = skipMany (noneOf "+-<>,.[]")
+
+brainfuck :: Text -> Text -> Maybe Text
+brainfuck program i = case parse parseProgram "" program of
+  Left _ -> Nothing
+  Right prog -> Just $ T.pack . D.toList . runProgram prog $ T.unpack i
+
+command :: CommandDef
+command = CommandDef { _verb   = ["bf"]
+                     , _help   = "<program> - run the given brainfuck program."
+                     , _action = go
+                     }
+  where
+    go [program] _ ev = return . reply ev $ case brainfuck program "" of
+      Nothing -> "Sorry, I don't understand that brainfuck program! Are brackets matched?"
+      Just res -> res
+    go (program:is) _ ev = return . reply ev $ case brainfuck program (T.unwords is) of
+      Nothing -> "Sorry, I don't understand that brainfuck program! Are brackets matched?"
+      Just res -> res
+    go _ _ _ = return $ return ()
--- yukibot/src/Yukibot/Plugins/Cellular.hs
+++ yukibot/src/Yukibot/Plugins/Cellular.hs
@@ -13,29 +13,14 @@ import Data.Word          (Word8)
 import Text.Read          (readMaybe)
 import Network.IRC.Asakura.Commands (CommandDef(..))
 import Network.IRC.Client (reply)
+import Yukibot.Utils.Types (Zipper(..), left, right)
 
 -- *Cellular automata
 
 -- |We can represent the state of the cellular automata as an infinite
 -- zip list, focussing at a single point.
-data Zipper a = Z [a] a [a]
-
--- |We can move left
-left :: Zipper a -> Zipper a
-left (Z (l:ls) a rs) = Z ls l (a:rs)
-
--- |And right
-right :: Zipper a -> Zipper a
-right (Z ls a (r:rs)) = Z (a:ls) r rs
-
-instance Functor Zipper where
-  fmap f (Z ls a rs) = Z (map f ls) (f a) (map f rs)
-
-instance Comonad Zipper where
-  extract (Z _ a _) = a
-  duplicate z = Z (tail $ iterate left z) z (tail $ iterate right z)
-
--- |Now we can apply a rule to the state, giving the new state as of
+--
+-- Now we can apply a rule to the state, giving the new state as of
 -- one step into the future.
 rule :: Word8 -> Zipper Bool -> Zipper Bool
 rule rnum state = state =>> rule'
