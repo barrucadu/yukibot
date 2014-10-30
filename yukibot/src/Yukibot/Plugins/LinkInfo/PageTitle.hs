7401f962dd02dc3bfa9b6dd83fd9b6a02c9efe09
Michael Walker <mike@barrucadu.co.uk>
2014-10-30 22:32:27 +0000

    Shamelessly steal Alan's title similarity metric from csbot

--- yukibot/src/Yukibot/Plugins/LinkInfo/PageTitle.hs
+++ yukibot/src/Yukibot/Plugins/LinkInfo/PageTitle.hs
@@ -3,10 +3,12 @@
 -- |Fetch and display titles for HTML pages.
 module Yukibot.Plugins.LinkInfo.PageTitle (pageTitle) where
 
-import Control.Monad (liftM)
-import Data.Monoid   ((<>))
-import Data.Text     (Text)
-import Network.URI   (URI(..))
+import Control.Applicative ((<$>))
+import Data.Char           (toLower)
+import Data.List           (isInfixOf, isPrefixOf)
+import Data.Monoid         ((<>))
+import Data.Text           (Text, unpack)
+import Network.URI         (URI(..), URIAuth(..))
 import Yukibot.Plugins.LinkInfo.Common (LinkHandler(..), LinkInfo(..), fetchTitle)
 
 import qualified Data.Text as T
@@ -19,12 +21,45 @@ pageTitle maxlen = LinkHandler
                    }
 
 -- |Try to fetch the title of a URL.
---
--- TODO: Don't return a title if it's too similar to the URI
 licHandler :: Int -> URI -> IO (LinkInfo Text)
-licHandler maxlen = liftM (fmap (trunc maxlen) . maybe NoTitle Title) . fetchTitle
+licHandler maxlen uri = do
+  title <- fmap (trunc maxlen) . maybe NoTitle Title <$> fetchTitle uri
+  return $ case title of
+    Title t | not $ isSimilar t uri -> title
+    _ -> NoTitle
 
 -- |Truncate a Text and append an ellipsis if too long
 trunc :: Int -> Text -> Text
 trunc maxlen txt | T.length txt > maxlen = T.take (maxlen - 1) txt <> "…"
 trunc _ txt = txt
+
+-- |Check that a title and uri aren't too similar
+isSimilar :: Text -> URI -> Bool
+isSimilar title uri = inDomain || inPath || inTitle
+  where
+    -- Normalise case and strip non-[a-z/]s
+    slug      = normalise $ unpack title
+    path      = normalise $ uriPath uri
+    domain    = maybe "" (normalise . uriRegName) $ uriAuthority uri
+    normalise = filter (`notElem` ('/':['a'..'z'])) . map toLower
+
+    -- Is the slug in the domain name?
+    inDomain = slug `isInfixOf` domain
+
+    -- Is the slug in the path?
+    inPath = slug `isInfixOf` path
+
+    -- Is some part of the path the start of the title?
+    inTitle = any (\c -> c `isPrefixOf` slug && sluglen <= length c * 2) chunks
+    chunks  = filter ((<10) . length) $ wordsWhen (=='/') path
+    sluglen = length slug
+
+-- |Split a string by a character
+--
+-- Why isn't this built in?
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =
+  case dropWhile p s of
+    "" -> []
+    s' -> let (w, s'') = break p s'
+         in w : wordsWhen p s''
