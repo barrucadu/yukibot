b2e3eda0c2312f22bef5fa7325452433f88c6a2e
Michael Walker <mike@barrucadu.co.uk>
2015-01-01 21:28:01 +0000

    Drop the credit/debt distinction, make 'pay' a synonym for 'owed', and implement mutual debt simplification

--- yukibot/src/Yukibot/Plugins/Dedebtifier.hs
+++ yukibot/src/Yukibot/Plugins/Dedebtifier.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Yukibot.Plugins.Dedebtifier
   ( -- *Commands
@@ -14,8 +12,7 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Arrow ((***))
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (MonadIO)
-import Data.Function (on)
-import Data.List (transpose)
+import Data.List (transpose, sort)
 import Data.Maybe (catMaybes, fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text, breakOn, stripPrefix, pack, unpack)
@@ -62,17 +59,9 @@ owedCmd mongo = CommandDef
 payCmd :: Mongo -> CommandDef
 payCmd mongo = CommandDef
   { _verb   = ["pay"]
-  , _help   = "pay <name> <amount> -- pay someone some money, this may put you in credit which will be redemmed automatically against future debts."
-  , _action = go
+  , _help   = "pay <name> <amount> -- pay someone some money, this is a synonym for 'owed'."
+  , _action = _action $ owedCmd mongo
   }
-
-  where
-    go [to, amount] _ ev = return $ case readCur amount of
-      Just amount' -> do
-        let User from = _source ev
-        addCredit mongo from to amount'
-      Nothing -> reply ev "Couldn't parse amount."
-    go _ _ ev = return $ reply ev "Expected two arguments."
 
 -- | List (simplified) debts in the system.
 listCmd :: Mongo -> CommandDef
@@ -85,19 +74,17 @@ listCmd mongo = CommandDef
   where
     go _ _ ev = do
       let User nick = _source ev
-      creditFrom <- queryMongo mongo (sel (Just "credit") (Just nick) Nothing Nothing) ["to"   =: (1::Int)]
-      creditTo   <- queryMongo mongo (sel (Just "credit") Nothing (Just nick) Nothing) ["from" =: (1::Int)]
-      debtFrom   <- queryMongo mongo (sel (Just "debt")   (Just nick) Nothing Nothing) ["to"   =: (1::Int)]
-      debtTo     <- queryMongo mongo (sel (Just "debt")   Nothing (Just nick) Nothing) ["from" =: (1::Int)]
+      from <- queryMongo mongo (sel (Just nick) Nothing Nothing) ["to"   =: (1::Int)]
+      to   <- queryMongo mongo (sel Nothing (Just nick) Nothing) ["from" =: (1::Int)]
 
-      let output = toBoxes . sort . listDebts $ creditFrom ++ creditTo ++ debtFrom ++ debtTo
+      let output = toBoxes . sort . listDebts $ from ++ to
 
       return $ case output of
         "" -> reply ev "You have no debts or credits."
         _ | '\n' `elem` output -> paste output >>= reply ev
           | otherwise -> reply ev $ pack output
 
-    listDebts = map $ \d -> [at' "from" "" d, at' "to" "" d, at' "type" "debt" d, showCur $ at' "amount" 0 d]
+    listDebts = map $ \d -> [at' "from" "" d, at' "to" "" d, showCur $ at' "amount" 0 d]
     toBoxes rows = render $ hsep 2 left (map (vcat left . map (text . unpack)) (transpose rows))
 
 -- * Managing debts
@@ -105,18 +92,9 @@ listCmd mongo = CommandDef
 -- | Add a new debt to the system, between the two people, for the
 -- given amount. Then simplify their debts/credits.
 addDebt :: MonadIO m => Mongo -> Text -> Text -> Integer -> m ()
-addDebt = flip add "debt"
-
--- | Add a new credit to the system, between the two people, for the
--- given amount. Then simplify their debts/credits.
-addCredit :: MonadIO m => Mongo -> Text -> Text -> Integer -> m ()
-addCredit = flip add "credit"
-
--- | Add a new debt/credit, adding to what was already there (if any).
-add :: MonadIO m => Mongo -> Text -> Text -> Text -> Integer -> m ()
-add mongo type_ from to amount = do
+addDebt mongo from to amount = do
   doMongo mongo $ \c -> do
-    let qS = sel (Just type_) (Just from) (Just to) Nothing
+    let qS = sel (Just from) (Just to) Nothing
 
     extant <- queryMongo mongo qS []
     deleteMongo mongo qS
@@ -125,28 +103,19 @@ add mongo type_ from to amount = do
           [e] -> at' "amount" 0 e
           _  -> 0
 
-    insert_ c $ mk type_ from to amount'
+    insert_ c $ debt from to amount'
 
   simplify mongo from
   simplify mongo to
 
 -- | Construct a debt record
 debt :: Text -> Text -> Integer -> Document
-debt = mk "debt"
+debt from to amount = sel (Just from) (Just to) (Just amount)
 
--- | Construct a credit record"
-credit :: Text -> Text -> Integer -> Document
-credit = mk "credit"
-
--- | Construct a debt/credit record
-mk :: Text -> Text -> Text -> Integer -> Document
-mk type_ from to amount = sel (Just type_) (Just from) (Just to) (Just amount)
-
--- | Construct a debt/credit record/selector
-sel :: Maybe Text -> Maybe Text -> Maybe Text -> Maybe Integer -> Document
-sel type_ from to amount = catMaybes
-  [ "type"   ~: type_
-  , "from"   ~: from
+-- | Construct a debt record/selector
+sel :: Maybe Text -> Maybe Text -> Maybe Integer -> Document
+sel from to amount = catMaybes
+  [ "from"   ~: from
   , "to"     ~: to
   , "amount" ~: amount]
 
@@ -157,29 +126,29 @@ sel type_ from to amount = catMaybes
 simplify :: MonadIO m => Mongo -> Text -> m ()
 simplify mongo who = doMongo mongo credits
   where
-    -- | Apply credits against debts
+    -- | Simplify mutual debts
     credits col = do
-      owes <- queryMongo mongo owesS   ["to" =: (1::Int)]
-      cred <- queryMongo mongo creditS ["to" =: (1::Int)]
+      owes <- queryMongo mongo owesS ["to"   =: (1::Int)]
+      cred <- queryMongo mongo owedS ["from" =: (1::Int)]
 
-      let mutuals = pairs' (txt "to") owes cred
+      let mutuals = pairs (\a b -> txt "to" a == txt "from" b) (\a b -> txt "to" a `compare` txt "from" b) owes cred
 
       forM_ mutuals $ \(d,c) -> do
         let d' = at' "amount" 0 d
         let c' = at' "amount" 0 c
 
-        let to = at' "to" "" d
+        let other = at' "to" "" d
 
         deleteMongo mongo d
         deleteMongo mongo c
 
         case () of
-          _ | c' > d'   -> insert_ col $ credit who to (c' - d')
-            | d' > c'   -> insert_ col $ debt   who to (d' - c')
+          _ | c' > d'   -> insert_ col $ debt other who (c' - d')
+            | d' > c'   -> insert_ col $ debt who other (d' - c')
             | otherwise -> return ()
 
-    owesS   = sel (Just "debt")   (Just who) Nothing Nothing
-    creditS = sel (Just "credit") (Just who) Nothing Nothing
+    owesS = sel (Just who) Nothing Nothing
+    owedS = sel Nothing (Just who) Nothing
 
     txt fld = at' fld (""::Text)
 
@@ -210,7 +179,3 @@ pairs eq cmp (a:as) (b:bs)
   | a `eq`  b      = (a,b) : pairs eq cmp as bs
   | a `cmp` b == GT = pairs eq cmp (a:as) bs
   | otherwise      = pairs eq cmp as (b:bs)
-
--- | Like pairs, but use (==) `on` and compare `on`.
-pairs' :: (Eq b, Ord b) => (a -> b) -> [a] -> [a] -> [(a,a)]
-pairs' f = pairs ((==) `on` f) (compare `on` f)
