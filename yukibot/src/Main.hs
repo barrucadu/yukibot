2c15c29740515cc13d212389df2a6846f3f2865c
Michael Walker <mike@barrucadu.co.uk>
2016-07-09 23:45:02 +0100

    Implement a seen plugin

--- yukibot/src/Main.hs
+++ yukibot/src/Main.hs
@@ -5,12 +5,15 @@ module Main where
 import System.Exit (die)
 
 import Yukibot.Backend.IRC (ircBackend)
+
 import Yukibot.Core
-import Yukibot.Plugin.Channel (channelPlugin)
-import Yukibot.Plugin.Hello (helloPlugin)
+
+import Yukibot.Plugin.Channel  (channelPlugin)
+import Yukibot.Plugin.Hello    (helloPlugin)
 import Yukibot.Plugin.LinkInfo (linkInfoPlugin)
-import Yukibot.Plugin.Mueval (muevalPlugin)
-import Yukibot.Plugin.Trigger (triggerPlugin)
+import Yukibot.Plugin.Mueval   (muevalPlugin)
+import Yukibot.Plugin.Seen     (seenPlugin)
+import Yukibot.Plugin.Trigger  (triggerPlugin)
 
 main :: IO ()
 main = case initialState of
@@ -23,5 +26,6 @@ initialState = addBackend "irc"     ircBackend     =<<
                addPlugin "hello"    helloPlugin    =<<
                addPlugin "linkinfo" linkInfoPlugin =<<
                addPlugin "mueval"   muevalPlugin   =<<
+               addPlugin "seen"     seenPlugin     =<<
                addPlugin "trigger"  triggerPlugin  =<<
                pure emptyBotState
