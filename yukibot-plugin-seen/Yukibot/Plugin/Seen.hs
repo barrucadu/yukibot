f73656f2433988acb435c38a51dc63f1dab11466
Michael Walker <mike@barrucadu.co.uk>
2016-11-05 23:01:14 +0000

    Add a basic quote plugin.

--- yukibot-plugin-seen/Yukibot/Plugin/Seen.hs
+++ yukibot-plugin-seen/Yukibot/Plugin/Seen.hs
@@ -17,21 +17,26 @@ module Yukibot.Plugin.Seen where
 
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.HashMap.Strict as H
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Time.Clock (UTCTime, getCurrentTime)
 import Data.Time.Format (defaultTimeLocale, formatTime)
-import Database.MongoDB ((=:), at)
+import Database.MongoDB (Selector, (=:), at)
 
 import Yukibot.Core
+import Yukibot.Extra
 
 seenPlugin :: config -> Either error Plugin
 seenPlugin _ = Right Plugin
   { pluginHelp = "keep track of the last thing said by a user"
   , pluginMonitors = H.fromList [("seen", seenMonitor)]
-  , pluginCommands = H.fromList [("seen", seenCommand)]
+  , pluginCommands = H.fromList [ ("seen", seenCommand)
+                                , ("quote-add",    quoteAddCommand)
+                                , ("quote-list",   quoteListCommand)
+                                , ("quote-remove", quoteRemoveCommand)
+                                ]
   }
 
 -- | Record everything said.
@@ -69,6 +74,95 @@ seenCommand = Command
     formatDoc :: UserName -> Maybe UTCTime -> Maybe Text -> Text
     formatDoc (UserName user) (Just when) (Just message) =
       user <> " was last seen at " <>
-      T.pack (formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" when) <>
+      showTime when <>
       " UTC saying '" <> message <> "'."
     formatDoc _ _ _ = "Something went wrong :("
+
+-- | Add a new quote.
+quoteAddCommand :: Command
+quoteAddCommand = Command
+  { commandHelp   = "add a new quote"
+  , commandAction = \ev args -> case eventChannel ev of
+      Just cname -> case args of
+        [who] -> do
+          seen <- listToMaybe <$> queryMongo [ "user" =: UserName who, "channel" =: cname ][]
+          case (,) <$> (seen >>= at "when") <*> (seen >>= at "message") of
+            Just (when, message) ->
+              addQuote cname (when::UTCTime) who (message::Text)
+            Nothing ->
+              reply $ "I haven't seen " <> who <> " say anything yet"
+        (who:what) -> do
+          now <- liftIO getCurrentTime
+          addQuote cname now who (T.unwords what)
+        _ -> reply "Who do you want me to quote?"
+      Nothing -> reply "Quotes only work in channels."
+  }
+  where
+    addQuote cname when who what = insertMongo
+      [[ "channel" =: cname
+       , "when"    =: when
+       , "quotee"  =: who
+       , "quote"   =: what
+       ]]
+
+-- | List all quotes in the current channel.
+quoteListCommand :: Command
+quoteListCommand = Command
+  { commandHelp   = "list quotes"
+  , commandAction = \ev args -> case eventChannel ev of
+      Just cname -> do
+        quotes <- findQuotes cname args
+        case quotes of
+          []  -> reply "There are no quotes."
+          [q] -> reply q
+          _   -> do
+            muri <- liftIO . paste . T.unpack $ T.unlines quotes
+            case muri of
+              Just uri -> reply $ "See " <> T.pack uri <> " for the quotes."
+              Nothing  -> reply "sprunge.us is not working right now."
+      Nothing -> reply "Quotes only work in channels."
+  }
+
+-- | Delete quotes in the current channel.
+quoteRemoveCommand :: Command
+quoteRemoveCommand = Command
+  { commandHelp   = "delete quotes"
+  , commandAction = \ev args -> case eventChannel ev of
+      Just cname
+        | null args -> reply "You need to give a query."
+        | otherwise -> do
+          quotes <- findQuotes cname args
+          muri   <- liftIO . paste . T.unpack $ T.unlines quotes
+          case (quotes, muri) of
+            ([], _)      -> reply "There are no quotes matching that query."
+            (_, Nothing) -> reply "sprunge.us is not working right now, so I'm not deleting those quotes."
+            (_, Just uri) -> do
+              reply $ "See " <> T.pack uri <> " for the deleted quotes."
+              deleteMongo (qfilter cname args)
+      Nothing -> reply "Quotes only work in channels."
+  }
+
+-- | Find and pretty-print quotes.
+findQuotes :: ChannelName -> [Text] -> BackendM [Text]
+findQuotes cname args = mapMaybe pprint <$> queryMongo (qfilter cname args) ["when" =: (1 :: Int)] where
+  pprint doc
+    | Just when   <- at "when"   doc
+    , Just quotee <- at "quotee" doc
+    , Just quote  <- at "quote"  doc
+    = Just $ showTime when <> " <" <> quotee <> "> " <> quote
+  pprint _ = Nothing
+
+-- | Selector for quotes in a channel.
+qfilter :: ChannelName -> [Text] -> Selector
+qfilter cname args =
+  [ "channel" =: cname
+  , "quotee"  =: ["$exists" =: True]
+  , "quote"   =: ["$exists" =: True]
+  , "$or" =: [ ["quotee" =: ("$regex" =: T.intercalate ".*" args)]
+             , ["quote"  =: ("$regex" =: T.intercalate ".*" args)]
+             ]
+  ]
+
+-- | Format a timestamp.
+showTime :: UTCTime -> Text
+showTime = T.pack . formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S"
